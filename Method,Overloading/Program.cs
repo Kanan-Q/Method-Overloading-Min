71fa0220f7ffe15ecec92b2985005571ebad3294
Kanan-Q <kananag-bp215@code.edu.az>
2024-10-19 20:28:28 +0400

    Min method

---
+++ Method,Overloading/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Method_Overloading;
+using static System.Console;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 0, 3, 8, 9, 4 };
+
+        int minElement = Element(array);
+
+        Write("Arrayin en kicik elementi: " + minElement);
+    }
+
+    static int Element(int[] arr)
+    {
+        int min = arr[0];
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
+
+
+
+
+
+
+
---
+++ Second/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Second;
+using static System.Console;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string sentence = "proqramlaşdırma dili";
+        string firstLetters = FirstLetters(sentence);
+        WriteLine("Sozlerin bas herfleri: " + firstLetters);
+    }
+
+    static string FirstLetters(string sentence)
+    {
+        string result = "";
+        bool Word = true;
+
+        for (int i = 0; i < sentence.Length; i++)
+        {
+
+            if (Word && sentence[i] != ' ')
+            {
+                result += sentence[i];
+                Word = false;
+                break;
+            }
+            else if (sentence[i] == ' ')
+            {
+                Word = true;
+            }
+        }
+
+        return result;
+    }
+}
